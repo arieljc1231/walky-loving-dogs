316e2f959ccc9698e4dba1f3ba5f04d492524978
Ariel Junior Camargo <ariel.321.09@gmail.com>
2025-06-22 19:23:32 -0300

    Ajusta api.js para diferenciar URLs de produção e dev

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://walkylovingdogs-437825ae9f55.herokuapp.com/api'
+  baseURL: 'https://walkylovingdogs-437825ae9f55.herokuapp.com/api', // Produção (Heroku)
+  // baseURL: 'http://localhost:5000/api',                         // Desenvolvimento local
 });
 
 export default api;
