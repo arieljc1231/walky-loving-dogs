29fb0c3a4f247d2b98a14d44bf03993c6bdb7e70
Ariel Junior Camargo <ariel.321.09@gmail.com>
2025-06-22 19:02:12 -0300

    Rollback api.js para URL de produção fixa

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,11 +1,8 @@
 // frontend/src/services/api.js
 import axios from 'axios';
 
-const baseURL =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:5000/api'                                    // -> seu Node/Express local
-    : 'https://walkylovingdogs-437825ae9f55.herokuapp.com/api';      // -> produção no Heroku
-
-const api = axios.create({ baseURL });
+const api = axios.create({
+  baseURL: 'https://walkylovingdogs-437825ae9f55.herokuapp.com/api'
+});
 
 export default api;
